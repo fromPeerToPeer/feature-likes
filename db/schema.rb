364a789108ab040eee96ba7a4d5bedd69bc84142
Drew Neil <andrew.jr.neil@gmail.com>
2015-10-26 20:26:18 +0000

    fixup! rails g model feature_request title granted:boolean

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151024175413) do
+ActiveRecord::Schema.define(version: 20151026201528) do
+
+  create_table "feature_requests", force: :cascade do |t|
+    t.string   "title"
+    t.boolean  "granted",    default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
