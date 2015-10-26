b3d1eba8afde0228ae6de285251491df9f0674b1
Drew Neil <andrew.jr.neil@gmail.com>
2015-10-26 20:28:46 +0000

    fixup! Create some seed FeatureRequests

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+nicky   = User.create(name: 'Nicky', email: 'nicky@peertopeer.io')
+melinda = User.create(name: 'Melinda', email: 'melinda@peertopeer.io')
+drew    = User.create(name: 'Drew', email: 'drew@peertopeer.io')
+
 keyboardFeature = FeatureRequest.create(title: 'Allow for keyboard-only use')
 mouseFeature    = FeatureRequest.create(title: 'Allow for mouse-only use')
 readerFeature   = FeatureRequest.create(title: 'Allow for screenreader use')
