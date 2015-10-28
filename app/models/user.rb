4c938d51332c05955087859af740ffc83c9f430e
Drew Neil <andrew.jr.neil@gmail.com>
2015-10-28 08:49:25 +0000

    Show "You have N likes to give" message to logged in users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,17 @@
 class User < ActiveRecord::Base
+  VOTES_ALLOWED = 2
+
   has_many :votes
 
   def voted_for?(feature)
     votes.any? { |v| v.feature_request == feature }
+  end
+
+  def votes_used
+    votes.count
+  end
+
+  def votes_remaining
+    VOTES_ALLOWED - votes_used
   end
 end
