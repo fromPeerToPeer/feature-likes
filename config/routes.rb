2f29805c82200e509701cf8f0bceabd981ca5e84
Drew Neil <andrew.jr.neil@gmail.com>
2015-11-06 11:49:57 +0000

    Create routes and placeholder pages for /about and /contact

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
   get '/logout', to: 'sessions#destroy'
 
   get '/features', to: 'features#index'
+  
+  get '/about', to: 'application#about'
+  get '/contact', to: 'application#contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
