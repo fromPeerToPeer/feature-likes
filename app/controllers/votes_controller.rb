339d06c7b21b39431c37d39bfd88ffd7308f1930
Drew Neil <andrew.jr.neil@gmail.com>
2015-11-13 12:43:10 +0000

    Make like/unlike buttons submit remote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,7 +2,11 @@ class VotesController < ApplicationController
   def create
     if feature = FeatureRequest.find(params[:feature_id])
       current_user.votes.create(feature_request: feature)
-      redirect_to features_path
+      @features = FeatureRequest.most_requested
+      respond_to do |format|
+        format.js { render partial: 'features/features', status: :created }
+        format.html { redirect_to features_path }
+      end
     end
   end
 
@@ -10,6 +14,10 @@ class VotesController < ApplicationController
     if vote = current_user.votes.where(feature_request_id: params[:feature_id]).first
       vote.destroy
     end
-    redirect_to features_path
+    @features = FeatureRequest.most_requested
+    respond_to do |format|
+      format.js { render partial: 'features/features' }
+      format.html { redirect_to features_path }
+    end
   end
 end
